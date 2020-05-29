d69d50028ccd087559b78016c4ed1ad7d9a4c260
yingjie <Yingjie_email@126.com>
2020-05-29 16:20:35 +0800

    aa

---
+++ SpringBoot/src/main/java/cn/cast/Application.java
@@ -0,0 +1,13 @@
+package cn.cast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan("cn.cast.mapper")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
